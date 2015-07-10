5b7934770dad381eb47065a93672518b493888d7
clcampbell <chris@clcampbell.com>
2015-07-10 16:28:56 -0400

    Fixed startup script bugs and added ssh config variables.

--- startup.sh
+++ startup.sh
@@ -3,13 +3,18 @@
 ln -s ~/.startup/abel.sh ~/abel.sh
 ln -s ~/.startup/redshift.sh ~/redshift.sh
 
-ln -s ~/.startup/.vimrc ~/.vimrc
+mv ~/.bashrc ~/.bashrc_bak
 ln -s ~/.startup/.bashrc ~/.bashrc
+ln -s ~/.startup/.vimrc ~/.vimrc
 ln -s ~/.startup/.conkyrc ~/.conkyrc
 ln -s ~/.startup/.Rprofile ~/.Rprofile
 ln -s ~/.startup/.screenrc ~/.screenrc
 
 mkdir ~/.ssh
 cp ~/.startup/sshconfig ~/.ssh/config
-chmod 700 .ssh/
-chmod 600 .ssh/*
+chmod 700 ~/.ssh/
+chmod 600 ~/.ssh/*
+
+printf "Host bx1\n    HostName 127.0.0.1\n    Port 2222\n    IdentityFile ~/.ssh/$HOSTNAME\nHost duro.einstein.yu.edu\n    IdentityFile ~/.ssh/$HOSTNAME\n" >> ~/.ssh/config
+
+
