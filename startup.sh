5329a4879356feb9d8c537b8556b75e7d36f482f
cflerin <ccflerin@gmail.com>
2019-03-13 16:29:19 +0100

    Added vim color profile to startup.sh

--- startup.sh
+++ startup.sh
@@ -5,9 +5,9 @@ ln -s ~/.startup/redshift.sh ~/redshift.sh
 mv ~/.bashrc ~/.bashrc_bak
 ln -s ~/.startup/.bashrc ~/.bashrc
 ln -s ~/.startup/.vimrc ~/.vimrc
-ln -s ~/.startup/.conkyrc ~/.conkyrc
+# ln -s ~/.startup/.conkyrc ~/.conkyrc
 ln -s ~/.startup/.Rprofile ~/.Rprofile
-ln -s ~/.startup/.screenrc ~/.screenrc
+# ln -s ~/.startup/.screenrc ~/.screenrc
 ln -s ~/.startup/.tmux.conf ~/.tmux.conf
 ln -s ~/.startup/.gitconfig ~/.gitconfig
 
@@ -21,4 +21,5 @@ chmod 600 ~/.ssh/*
 #printf "Host bx1\n    HostName 127.0.0.1\n    Port 2222\n    IdentityFile ~/.ssh/$HOSTNAME\nHost duro.einstein.yu.edu\n    IdentityFile ~/.ssh/$HOSTNAME\n" >> ~/.ssh/config
 
 mkdir -p ~/.vim/nerdtree_plugin/ && cp yank_mapping.vim ~/.vim/nerdtree_plugin/yank_mapping.vim
+mkdir -p ~/.vim/colors/ && wget -P ~/.vim/colors/ https://raw.githubusercontent.com/romainl/flattened/master/colors/flattened_dark.vim 
 
